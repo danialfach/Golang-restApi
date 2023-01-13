a6f732792e2f613c2a44101a8b7a3ecfb91e6380
danialfach <danialfacrudin@gmail.com>
2023-01-13 13:30:30 +0700

    golang restAPi

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	productcontroller "github.com/danialfach/go-restapi-gin/controllers/productController"
+	"github.com/danialfach/go-restapi-gin/models"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	models.ConnectDatabase()
+
+	r.GET("/api/products", productcontroller.Index)
+	r.GET("/api/products/:id", productcontroller.Show)
+	r.POST("/api/product", productcontroller.Create)
+	r.PUT("/api/product/:id", productcontroller.Update)
+	r.DELETE("/api/product", productcontroller.Delete)
+
+	r.Run()
+}
